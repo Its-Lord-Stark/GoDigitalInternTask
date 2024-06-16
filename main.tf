67b95685c39731326ebbc48621aa50d24a1a28bf
Stark <karneatharv1920@gmail.com>
2024-06-16 12:25:19 +0530

    mydb change 1

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ resource "aws_db_instance" "mydb" {
   parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
   publicly_accessible    = true 
+
+  tags = {
+    Name = "mydb"
+  }
 }
 
 
